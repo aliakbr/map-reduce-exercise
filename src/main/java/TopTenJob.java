aa3c49c3e37c78f3d1726bbc70d3c30c951a9bc7
Ali Akbar <aliraal12@gmail.com>
2017-11-27 22:01:21 +0700

    Fix TopTenJob script

--- src/main/java/TopTenJob.java
+++ src/main/java/TopTenJob.java
@@ -1,55 +1,56 @@
 import java.util.*;
 
 import java.io.IOException;
-import java.io.IOException;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class TopTenJob {
+
     //Mapper class
-    public static class top_ten_map extends MapReduceBase implements
-            Mapper<Object, Text, NullWritable, Text> {
+    public static class top_ten_map extends Mapper<Object, Text, NullWritable, Text> {
         private TreeMap<Double, Text> followers = new TreeMap<Double, Text>();
 
-        public void map(Object o, Text value, OutputCollector<NullWritable, Text> outputCollector, Reporter reporter) throws IOException {
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+
             // Split on tab to get the cat name and the weight
             String v[] = value.toString().split("\t");
             Double total = Double.parseDouble(v[1]);
-
-            followers.put(total, value);
+            followers.put(new Double(total), new Text(value));
 
             if (followers.size() > 10) {
                 followers.remove(followers.firstKey());
             }
         }
 
-        protected void cleanup(org.apache.hadoop.mapreduce.Mapper.Context context)
+        protected void cleanup(Context context)
                 throws IOException, InterruptedException {
 
-            for ( Text id_user : followers.values() ) {
-                context.write(NullWritable.get(),id_user);
+            for ( Text a : followers.values() ) {
+                context.write(NullWritable.get(), a);
             }
         }
     }
 
 
     //Reducer class
-    public static class top_ten_reducer extends MapReduceBase implements
-            Reducer<NullWritable, Text, NullWritable, Text>
-    {
+    public static class top_ten_reducer extends Reducer<NullWritable, Text, NullWritable, Text> {
+        public void reduce(NullWritable key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
 
-        public void reduce(NullWritable nullWritable, Iterator<Text> values, OutputCollector<NullWritable, Text> outputCollector, Reporter reporter) throws IOException {
             TreeMap<Double, Text> followers = new TreeMap< Double, Text>();
 
-            while (values.hasNext()) {
-                Text value = values.next();
+            for (Text value : values) {
                 String v[] = value.toString().split("\t");
                 Double weight = Double.parseDouble(v[1]);
-                followers.put(weight, value);
+                followers.put(new Double(weight), new Text(value));
 
                 if (followers.size() > 10) {
                     followers.remove(followers.firstKey());
@@ -57,7 +58,7 @@ public class TopTenJob {
             }
 
             for (Text t : followers.values()) {
-                outputCollector.collect(NullWritable.get(), new Text(t));
+                context.write(NullWritable.get(), t);
             }
         }
     }
@@ -66,8 +67,7 @@ public class TopTenJob {
     //Main function
     public static void main(String args[])throws Exception
     {
-
-        JobConf conf = new JobConf(Job2.class);
+        Configuration conf= new Configuration();
         conf.set("mapred.child.java.opts", "-Xmx2048m");
         conf.set("mapreduce.map.java.opts", "-Xmx2048m");
         conf.set("mapreduce.map.memory.mb", "4096");
@@ -75,16 +75,30 @@ public class TopTenJob {
         conf.set("mapreduce.reduce.java.opts", "-Xmx2048m");
         conf.set("mapreduce.reduce.shuffle.input.buffer.percent", "0.2");
         conf.set("mapreduce.reduce.memory.mb", "4096");
-        conf.setJobName("af_job_3");
-        conf.setOutputKeyClass(NullWritable.class);
-        conf.setOutputValueClass(Text.class);
-        conf.setMapperClass(TopTenJob.top_ten_map.class);
-//        conf.setCombinerClass(reducer1.class);
-        conf.setReducerClass(TopTenJob.top_ten_reducer.class);
-        conf.setInputFormat(TextInputFormat.class);
-        conf.setOutputFormat(TextOutputFormat.class);
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-        JobClient.runJob(conf);
+
+        Job job = new Job(conf,"af_top_ten");
+        job.setJarByClass(TopTenJob.class);
+
+        //Providing the mapper and reducer class names
+        job.setMapperClass(TopTenJob.top_ten_map.class);
+        job.setReducerClass(TopTenJob.top_ten_reducer.class);
+
+        job.setNumReduceTasks(1);
+
+        //Setting
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        Path outputPath = new Path(args[1]);
+
+
+        //Configure input/output path
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        //Exiting the job if flag is false
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
